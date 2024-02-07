52dd60f2e1997dfc3b4f64ace00f1a98e259f75d
sabbir ahmmed <ahsabbir103@gmail.com>
2024-02-07 09:38:45 +0600

    change dashboard icons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -9,6 +9,8 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { FreeCounter } from "@/components/free-counter";
 
+import logoImg from "@/app/images/white.png"
+
 const poppins = Montserrat ({ weight: '600', subsets: ['latin'] });
 
 const routes = [
@@ -69,10 +71,10 @@ export const Sidebar = ({
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative h-8 w-8 mr-4">
-            <Image fill alt="Logo" src="/logo.png" />
+            <Image fill alt="Logo" src={logoImg} />
           </div>
           <h1 className={cn("text-2xl font-bold", poppins.className)}>
-            Genius
+            KOODI AI
           </h1>
         </Link>
         <div className="space-y-1">
