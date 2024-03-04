0e9e37444be1f8de2d7fc179ab82cb33252137aa
sabbir ahmmed <ahsabbir103@gmail.com>
2024-03-04 19:11:45 +0600

    error in code generator.

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -9,9 +9,13 @@ import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 //  apiKey: process.env.OPENAI_API_KEY,
 //});
 
+
+
+
+
 const openrouter = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env.OPENROUTER_API_KEY,
+  baseURL: "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1",
+  apiKey: process.env.HUGGINGFACE_API_KEY,
   defaultHeaders: {
     "HTTP-Referer": process.env.YOUR_SITE_URL, // Optional, for including your app on openrouter.ai rankings.
     "X-Title": process.env.YOUR_SITE_NAME, // Optional. Shows in rankings on openrouter.ai.
@@ -21,10 +25,21 @@ const openrouter = new OpenAI({
 
 //const openai = new OpenAIApi(configuration);
 
-const instructionMessage: any = {
+const payload: any = [{
   role: "system",
   content: "You are an expert software engineer and software developer you can solve and write clean code as per other's needs. also, you're a code generator too. You must answer only in markdown code snippets. Use code comments for explanations."
-};
+}];
+
+// Add your token from https://huggingface.co/settings/token
+// const Req_body= {
+//   headers:{"Authorization": "Bearer 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"},
+//   wait_for_model: true,
+//   use_gpu: false,
+//   method : "POST",
+//   contentType : "application/json",
+//   payload: ""
+// };
+
 
 export async function POST(
   req: Request
@@ -33,6 +48,14 @@ export async function POST(
     const { userId } = auth();
     const body = await req.json();
     const { messages  } = body;
+
+    const quest = {
+      role: "user",
+      content: [...messages]
+    }
+
+    // payload.push(payload)
+    // Req_body.payload = JSON.stringify(payload)
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -53,16 +76,26 @@ export async function POST(
       return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
     }
 
-    const response = await openrouter.chat.completions.create({
-      model: "openrouter/auto",
-      messages: [instructionMessage, ...messages]
-    });
+    // const response = await openrouter.chat.completions.create({
+    //   model: "openrouter/auto",
+    //   messages: [instructionMessage, ...messages]
+    // });
+
+    const response = await fetch("https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1",{
+      method:'POST',
+      headers: {
+        "Authorization": "Bearer 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",
+        "contentType" : "application/json"
+      },
+      body:JSON.stringify({inputs:"I forgot how to kill a process in Linux, can you help?"})
+
+    }).then(res=> console.log(res.json()))
 
     if (!isPro) {
       await incrementApiLimit();
     }
-    console.log('[CODE_API_RESPONSE]', response)
-    return NextResponse.json(response.choices[0].message);
+    // console.log('[CODE_API_RESPONSE]', response.json())
+    return NextResponse.json("response.choices[0].message");
   } catch (error) {
     console.log('[CODE_ERROR]', error);
     return new NextResponse("Internal Error", { status: 500 });
