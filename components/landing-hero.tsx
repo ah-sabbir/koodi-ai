a0486c36086c3992ef30598149e5568e79a6f126
sabbir ahmmed <ahsabbir103@gmail.com>
2024-03-08 19:12:50 +0600

    updated landing page

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,7 +12,7 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#6A65F2] to-[#b573f8]">The Best AI Tool for</h1>
+        <h1 className="text-transparent bg-clip-text bg-gradient-to-b font-semibold from-[#f5f5ff] to-[#98a09b] uppercase">Build Your Own AI APP, No code.</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 pb-5">
           <TypewriterComponent
             options={{
@@ -20,7 +20,12 @@ export const LandingHero = () => {
                 "Chatbot.",
                 "Photo Generation.",
                 "Blog Writing.",
-                "Mail Writing."
+                "eMail Writing.",
+                "Content Writing.",
+                "Marketing.",
+                "Product Research.",
+                "Market Research.",
+                "Build More."
               ],
               autoStart: true,
               loop: true,
@@ -29,12 +34,12 @@ export const LandingHero = () => {
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-100">
-        Create content using AI 10x faster.
+      Build and use with just one click for any industry needs.
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
-            Start Generating For Free
+            Start Now For Free
           </Button>
         </Link>
       </div>
--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -7,25 +7,46 @@ import { useAuth } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Each } from "./ui/Each";
 
-const font = Montserrat({ weight: '600', subsets: ['latin'] });
+const font = Montserrat({ weight: '400', subsets: ['latin'] });
+
+const navItems = [
+  {name:'Home', url: ''},
+  {name: 'Apps', url:'apps'},
+  {name: 'Pricing', url:'pricing'},
+  {name: 'Features', url:'features'},
+]
+
+// ##5946CF
 
 export const LandingNavbar = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <nav className="p-4 bg-transparent flex items-center justify-between">
+    <nav className="p-4 bg-[#221e3b9f] flex items-center justify-between rounded-full mt-5 drop-shadow-md">
       <Link href="/" className="flex items-center">
-        <div className="relative h-8 w-8 mr-4">
+        <div className="relative h-12 w-12">
           <Image fill alt="Logo" src="/app-logo.png" />
         </div>
-        <h1 className={cn("text-2xl font-bold text-white", font.className)}>
+        {/* <h1 className={cn("text-2xl font-bold text-white", font.className)}>
           KOODI AI
-        </h1>
+        </h1> */}
       </Link>
-      <div className="flex items-center gap-x-2">
+      <div className="nav-items hidden sm:block md:block lg:block xl:block">
+        <ul className="flex flex-row gap-x-5 text-slate-300">
+          <Each of={navItems} render={(item:any)=>(
+              <li className="" onClick={(e)=> console.log("clicked!")}>
+                <Link href={`/${item.url}`} >{item.name}</Link> 
+                {/* className=" border-2 border-[#5846cf96] rounded-md p-2" */}
+              </li>
+            )}/>
+        </ul>      
+      </div>
+
+      <div className="flex items-center gap-x-2 ">
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="outline" className="rounded-full">
+          <Button className="bg-gradient-to-r from-purple-400 to-indigo-500 text-white font-semibold py-2 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out">
             Get Started
           </Button>
         </Link>
