5e133e072d4825c4735cb716581033209021534c
sabbir ahmmed <ahsabbir103@gmail.com>
2024-03-09 21:41:08 +0600

    need to add chat api

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -112,8 +112,9 @@ export async function POST(
       // url: "https://huggingface.co/chat/api/conversation",
       headers: {Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`},
       data: {
-        inputs: JSON.stringify(`Please ignore all previous instructions. I want you to only answer in English. Please answer the following question about the opened page content to the best of your ability and provided context. Be precise and helpful. Do not hallucinate and do not come up with facts you are not sure about. Avoid mentioning context as incomplete. [CONTEXT]: programming. [QUESTION]: ${messages.slice(-1)[0].content} [ANSWER]:`)
-      }
+        inputs: JSON.stringify(`Please ignore all previous instructions. I want you to only answer in English. Please answer the following question about the opened page content to the best of your ability and provided context. Be precise and helpful. Do not hallucinate and do not come up with facts you are not sure about. elaborate the answer as much possible. [CONTEXT]: programming. [QUESTION]: ${messages.slice(-1)[0].content} [ANSWER]:`)
+      },
+      // responseType: 'stream'
     })
 
 
@@ -141,7 +142,7 @@ export async function POST(
 
     // const content = result[0].generated_text.replace(`Please ignore all previous instructions. I want you to only answer in English. Please answer the following question about the opened page content to the best of your ability and provided context. Be precise and helpful. Do not hallucinate and do not come up with facts you are not sure about. Avoid mentioning context as incomplete. [CONTEXT]: programming. [QUESTION]: ${messages.slice(-1)[0].content} [ANSWER]:`,``).trim()
 
-    console.log('[CODE_API_RESPONSE]', )
+    console.log('[CODE_API_RESPONSE]', "result")
     return NextResponse.json({role:'bot', content: result[0].generated_text});
   } catch (error) {
     console.log('[CODE_ERROR]', error);
